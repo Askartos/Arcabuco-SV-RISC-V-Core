4368083515d263ee5c13116c2a9973ce0dbbfbbd
Askartos <hanssel1301@gmail.com>
2024-12-15 14:28:21 +0100

    adding new enum

--- src/rtl/arcabuco_core/arcabuco_core_pack.sv
+++ src/rtl/arcabuco_core/arcabuco_core_pack.sv
@@ -1,4 +1,6 @@
 package arcabuco_core_pack;
 typedef enum logic [3:0]{ alu_sll, alu_srl,  alu_sra, alu_add, alu_sub, alu_xor,  alu_or,   alu_and, alu_slt,
                           alu_sltu, alu_eq,  alu_neq, alu_grt, alu_grtu, alu_nop1, alu_nop2} t_alu_opcode;
+
+typedef enum logic [1:0]{ mul, div, nop} t_muldiv_opcode;
 endpackage
